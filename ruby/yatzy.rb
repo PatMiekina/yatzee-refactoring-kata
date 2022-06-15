d79ccba1cee5652379656378574acd0194847f31
Patrycja Miekina <patrycja.miekina@patrycjas-mbp.home>
2022-06-15 12:11:30 +0100

    #ones refactored

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -23,25 +23,8 @@ class Yatzy
   end
 
   def self.ones( d1,  d2,  d3,  d4,  d5)
-    sum = 0
-    if (d1 == 1)
-      sum += 1
-    end
-    if (d2 == 1)
-      sum += 1
-    end
-    if (d3 == 1)
-      sum += 1
-    end
-    if (d4 == 1)
-      sum += 1
-    end
-    if (d5 == 1)
-      sum += 1
-    end
-
-    sum
-
+    array = [d1, d2, d3, d4, d5].to_a
+    array.count(1)
   end
 
   def self.twos( d1,  d2,  d3,  d4,  d5)
