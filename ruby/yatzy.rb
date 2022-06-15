40b6cb139c2dba781f69862bd23971760a663226
Patrycja Miekina <patrycja.miekina@patrycjas-mbp.home>
2022-06-15 15:30:22 +0100

    refactored #four_of_a_kind

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -92,19 +92,24 @@ class Yatzy
     end
   end
 
-  def self.four_of_a_kind( _1,  _2,  d3,  d4,  d5)
-    tallies = [0]*6
-    tallies[_1-1] += 1
-    tallies[_2-1] += 1
-    tallies[d3-1] += 1
-    tallies[d4-1] += 1
-    tallies[d5-1] += 1
-    for i in (0..6)
-      if (tallies[i] >= 4)
-        return (i+1) * 4
-      end
+  def self.four_of_a_kind(d1,  d2,  d3,  d4,  d5)
+    # tallies = [0]*6
+    # tallies[d1-1] += 1
+    # tallies[d2-1] += 1
+    # tallies[d3-1] += 1
+    # tallies[d4-1] += 1
+    # tallies[d5-1] += 1
+    # for i in (0..6)
+    #   if (tallies[i] >= 4)
+    #     return (i+1) * 4
+    #   end
+    # end
+    # return 0
+    dice = [d1, d2, d3, d4, d5]
+    for i in 1..6 do
+      return 4 * i if dice.count(i) >= 4
     end
-    return 0
+    0
   end
 
   def self.three_of_a_kind( d1,  d2,  d3,  d4,  d5)
