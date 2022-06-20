0884e8830522b8d898efb80a2302117bba5f865b
Patrycja Miekina <patrycja.miekina@patrycjas-mbp.home>
2022-06-20 14:22:53 +0100

    refactored input to *dice)

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -1,12 +1,11 @@
 # ALL METHODS TO HAVE THE SAME INPUT!
 class Yatzy
   
-  def self.chance(d1, d2, d3, d4, d5)
-    [d1, d2, d3, d4, d5].sum
+  def self.chance(*dice)
+    dice.sum
   end
 
-  def self.yatzy(d1, d2, d3, d4, d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.yatzy(*dice)
     score = 0
     for i in 1..6 do
         score = 50 if dice.count(i) == 5
@@ -18,8 +17,7 @@ class Yatzy
     dice_set.count(dice) * dice
   end
 
-  def self.pair( d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.pair(*dice)
     score = 0
     for i in 1..6 do
       score = 2 * i if dice.count(i) >= 2
@@ -27,8 +25,7 @@ class Yatzy
     score
   end
 
-  def self.four_of_a_kind(d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.four_of_a_kind(*dice)
     score = 0
     for i in 1..6 do
       score = 4 * i if dice.count(i) >= 4
@@ -36,8 +33,7 @@ class Yatzy
     score
   end
 
-  def self.three_of_a_kind( d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.three_of_a_kind(*dice)
     score = 0
     for i in 1..6 do
       score = 3 * i if dice.count(i) >= 3
@@ -45,8 +41,7 @@ class Yatzy
     score
   end
 
-  def self.two_pairs(d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.two_pairs(*dice)
     for i in 1..6 do
       pair1 = i if dice.count(i) >= 2
     end
@@ -57,20 +52,17 @@ class Yatzy
     pair1 != 0 && pair2 != 0 ? 2 * pair1 + 2 * pair2 : 0
   end
 
-  def self.small_straight(d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.small_straight(*dice)
     return 15 if dice.sort == (1..5).to_a
     0
   end
 
-  def self.large_straight(d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.large_straight(*dice)
     return 20 if dice.sort == (2..6).to_a
     0
   end
 
-  def self.full_house(d1,  d2,  d3,  d4,  d5)
-    dice = [d1, d2, d3, d4, d5]
+  def self.full_house(*dice)
     three = 0
     pair = 0
     for i in 1..6 do
@@ -81,5 +73,16 @@ class Yatzy
     pair = i if dice.count(i) >= 2
     end
     three && pair ? 3 * three + 2 * pair : 0
-end
+
+    # call #three_of_a_kind and then call on #pair
+    # problem -> input of #pair -> changed from d1, d2 etc to *dice (moves into array)
+    # threes = self.three_of_a_kind(dice) / 3
+    # if threes != 0
+    #   3.times {dice.delete(threes)} 
+    #   pair = self.pair(dice[0..2]) / 2
+    #   return 3* threes + pair * 2 if pair != 0
+    # else
+    #   return 0
+    # end
+  end
 end
